2f8eaafde9d51e38faca910028d6d173489d9862
Ximena Morgado <ximenamorgado@MacBook-Air-de-Ximena.local>
2018-03-16 10:47:24 -0600

    Smash 2 version

--- Thesis/tesis/EvoMining/ARCHAEAS/GetSMASHonconcat.pl
+++ Thesis/tesis/EvoMining/ARCHAEAS/GetSMASHonconcat.pl
@@ -5,42 +5,56 @@ use strict;
 my $fileCENTRAL=$ARGV[0]; ##Central.numbers
 ## tab separated family_number  family_Name
 
-my $fileSMASH=$ARGV[1]; ##Central.numbers
+my $fileSMASH=$ARGV[1]; ##SMASH file
 
 
 open (FILE,$fileCENTRAL) or die;
 
-getSMASH();
+my @SMASH=getSMASH($fileSMASH);
+print "SMASH load finished";
+my $pause=<STDIN>;
+
 my $fasta;
 
 foreach my $line (<FILE>){
 	chomp $line;
 	my @st=split(/\t/,$line);
 	 $fasta=$st[0].".concat.fasta";
-	getSMASHonTree($fasta,$fileSMASH);
+	print"$st[1]\t";
+	getSMASHonTree($fasta,@SMASH);
 		}
 
-sub getSMASH7
-sub getSMASHonTree{
-	my $fileFasta=shift;
+sub getSMASH{
 	my $fileSMASH=shift;
-	#buscar los Elementos de BBH que estan en fasta
-
 	my  @SMASH;
-	my @FASTA;
-	open (FILE,">$fasta\.smash");
-	#Read BBBH file
-	open (SMASH,$fileSMASH) or die;
 
-	foreach my $line (<SMASH>){
+	open (FSMASH,$fileSMASH) or die;
+
+	foreach my $line (<FSMASH>){
 		chomp $line;
 		my@parts=split(/\t/,$line);
-#		print "#$parts[1]#\n";
-#		my $pause=<STDIN>;
+		##print "#$parts[1]#\n";
+		#my $pause=<STDIN>;
 		if(!($parts[1]~~@SMASH)){
 			push(@SMASH,$line);
 			}
 		}	
+	close FSMASH;
+	return @SMASH;
+
+}
+
+
+sub getSMASHonTree{
+	my $fileFasta=shift;
+
+	my @SMASH=@_;
+
+	#buscar los Elementos de BBH que estan en fasta
+
+	my @FASTA;
+	open (FILE,">$fileFasta\.smash");
+	#Read BBBH file
 
 	#Read fasta file
 	open (FASTA,$fileFasta) or die;
@@ -50,6 +64,7 @@ sub getSMASHonTree{
 		chomp $line;
 		if ($line=~/>/ and $line=~/gi/ ){
 		my @st=split(/\|/,$line);
+		#print "$st[1]\n";
 		if($st[1]~~@SMASH){
 			$Count++;
 			print FILE "$st[1]\n";
